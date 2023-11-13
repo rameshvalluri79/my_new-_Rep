69117e4a64afa9f51cfadb964d24982b59072293
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 01:48:37 +0530

    Update my_new_file.py
    removed stop

--- my_new_file.py
+++ my_new_file.py
@@ -31,8 +31,6 @@ streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
 streamlit.error()
 
-streamlit.stop()
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
