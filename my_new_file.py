8fb0a1b1353884e4c4492fea08771d24264fa40c
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 01:17:14 +0530

    Update my_new_file.py
    new line for interaction

--- my_new_file.py
+++ my_new_file.py
@@ -37,4 +37,5 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values('From Streamlit')")
 
