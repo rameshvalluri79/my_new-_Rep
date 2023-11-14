b81181335d6eb35d122493feb5d30f43a8794e4c
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 20:30:08 +0530

    Update my_new_file.py
    try try

--- my_new_file.py
+++ my_new_file.py
@@ -15,6 +15,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Kiwifruit','Pineapple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
 #create the repeatable code block (called a function) 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
