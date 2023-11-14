52a84da0a457b01ad82f3897a2777cc389c0a5bb
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 20:06:56 +0530

    Update my_new_file.py
    try

--- my_new_file.py
+++ my_new_file.py
@@ -31,8 +31,16 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice) 
     streamlit.dataframe(back_from_function)
 
+streamlit.error()
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
-
-
-
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values('From Streamlit')")
