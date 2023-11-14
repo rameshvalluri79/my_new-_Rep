0b6a212665c20bcebfed72738d8a48290cc525b2
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 19:18:56 +0530

    Update my_new_file.py
    added updated

--- my_new_file.py
+++ my_new_file.py
@@ -24,6 +24,19 @@ else:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-except Exception as e:
+except URLError as e:
 streamlit.error()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+my_cur.execute("insert into fruit_load_list values('From Streamlit')")
+
 
