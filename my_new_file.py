ad76471ea6a6a5e8d753a8a2ff27a25cee43f527
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 19:48:35 +0530

    Update my_new_file.py
    space removed

--- my_new_file.py
+++ my_new_file.py
@@ -15,12 +15,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Kiwifruit','Pineapple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
- 
 def get_fruitvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
+ 
 streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
