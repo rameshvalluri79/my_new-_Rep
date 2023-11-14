7b10bad3e4cf48d56acd65074e5f3e14906f8c51
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-15 00:13:37 +0530

    Update my_new_file.py
    added insert code

--- my_new_file.py
+++ my_new_file.py
@@ -16,17 +16,18 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("select a fruit to get info")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-    
-except URLError as e:
-  streamlit.error()
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake (new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
+         return "Thanks for adding + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
+if streamlit.button('Add a Fruit to the List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets ["snowflake"]) 
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
+
 streamlit.header("The fruit load list contains:") 
 #Snowflake-related functions
 def get_fruit_load_list():
