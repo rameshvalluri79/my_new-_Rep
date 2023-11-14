e64f4ad94e30d44940234e4ac68682b3199edac8
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 19:54:29 +0530

    Update my_new_file.py
    added error line

--- my_new_file.py
+++ my_new_file.py
@@ -15,12 +15,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Kiwifruit','Pineapple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
 def get_fruitvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
  
-streamlit.header('Fruityvice Fruit Advice!')
+    streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
@@ -28,6 +29,7 @@ try:
     else:
         back_from_function = get_fruitvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
+        streamlit.error()
 
 
 
