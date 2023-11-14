f3f86352176aabcf61686acdc76e8671a1b6acfb
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 23:57:46 +0530

    Update my_new_file.py
    stop added

--- my_new_file.py
+++ my_new_file.py
@@ -39,7 +39,7 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe (my_data_rows)
-
+streamlit.stop()
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
