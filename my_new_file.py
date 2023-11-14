946aab1f2f12706979f7353d2827a5230631f1d5
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 23:27:04 +0530

    Update my_new_file.py
    Back to old code

--- my_new_file.py
+++ my_new_file.py
@@ -15,21 +15,18 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Kiwifruit','Pineapple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
-#create the repeatable code block (called a function) 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-    
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?') 
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice) 
-        streamlit.dataframe(back_from_function)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+streamlit.error("select a fruit to get info")
+else:
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
