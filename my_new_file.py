8263bba67ff082ce0609d19194991cefab64a861
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-14 18:11:50 +0530

    Update my_new_file.py
    removed streamlit.stop()

--- my_new_file.py
+++ my_new_file.py
@@ -24,7 +24,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the json version
@@ -32,7 +31,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
