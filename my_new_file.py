12322eb6ffa7d881bcf2a51bec54c6ff8153241b
rameshvalluri79 <150635618+rameshvalluri79@users.noreply.github.com>
2023-11-15 00:16:31 +0530

    Update my_new_file.py
    error cleared

--- my_new_file.py
+++ my_new_file.py
@@ -20,7 +20,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 def insert_row_snowflake (new_fruit):
     with my_cnx.cursor() as my_cur:
          my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
-         return "Thanks for adding + new_fruit
+         return "Thanks for adding" + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
 if streamlit.button('Add a Fruit to the List'):
@@ -34,13 +34,13 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
-streamlit.stop()
+
 # Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe (my_data_rows)
-
+streamlit.stop()
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
